30a9af64d48d46fbbc39574f5de59a340914956a
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2023-11-13 09:59:16 +0100

    Update colorscheme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,14 +1,14 @@
 -- [[ Configure colorscheme ]]
-require("gruvbox").setup({
-  italic = {
-    strings = false,
-    emphasis = false,
-    comments = true,
-    operators = false,
-    folds = true,
-  },
-})
-
-vim.cmd("colorscheme gruvbox")
+-- require("gruvbox").setup({
+--   italic = {
+--     strings = false,
+--     emphasis = false,
+--     comments = true,
+--     operators = false,
+--     folds = true,
+--   },
+-- })
+--
+vim.cmd("colorscheme tokyonight-moon")
 
 -- vim: ts=2 sts=2 sw=2 et
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -97,7 +97,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'gruvbox',
+        theme = 'tokyonight',
         -- component_separators = '|',
         -- section_separators = '',
       },
